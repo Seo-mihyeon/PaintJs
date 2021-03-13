be5011b1419c3e2bae2e8fedf7714cba284b706d
Seo-mihyeon <63391225+Seo-mihyeon@users.noreply.github.com>
2021-03-13 16:28:00 +0900

    펜 색깔 및 두께

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -36,9 +38,27 @@ function onMouseDown(event) {
   painting = true;
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor; //클릭한 색깔의 정보 가져오기
+  ctx.strokeStyle = color; //선택한 색상으로 기본값이 바뀐다.
+}
+
+function handleRangeChange(event) {
+  const stroke = event.target.value;
+  ctx.lineWidth = stroke;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove); // canvas 안에 마우스 움직임시 호출처리
   canvas.addEventListener("mousedown", startPainting); //canvas 안에서 마우스 클릭
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
 }
